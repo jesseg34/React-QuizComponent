596239048794f000461187d8c747e5fecba9dc99
Jesse Greenfield <jesseg@columbiacu.org>
2019-12-17 14:03:36 -0800

    adding wrong answer text

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -2,14 +2,28 @@ import React, { Component } from 'react'
 import QuizQuestionButton from './QuizQuestionButton'
 
 class QuizQuestion extends Component {
+    constructor(props) {
+        super(props)
+        this.state = { incorrectAnser: false }
+    }
+
     handleClick(buttonText) {
         if (buttonText === this.props.quiz_question.answer) {
             this.props.showNextQuestionHandler()
+            this.setState((state) => {
+                return { incorrectAnser: false }
+            })
+        } else {
+            this.setState((state) => {
+                return { incorrectAnser: true }
+            })
         }
     }
+
     render() {
         return (
             <main>
+                {this.state.incorrectAnser ? <p className='error'>Sorry, that's not right</p> : null}
                 <section>
                 <p>{this.props.quiz_question.instruction_text}</p>
                 </section>
