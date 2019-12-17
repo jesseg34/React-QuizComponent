b6f499df104516c4a99f04e95789be7e6449259b
Jesse Greenfield <jesseg@columbiacu.org>
2019-12-17 13:52:24 -0800

    adding answer logic

--- src/Quiz.js
+++ src/Quiz.js
@@ -10,13 +10,22 @@ class Quiz extends Component {
         this.state = { quiz_position: 1 }
     }
 
+    showNextQuestion() {
+        this.setState((state) => {
+            return { quiz_position: state.quiz_position + 1 }
+        })
+    }
+
     render() {
         const isQuizEnd = this.state.quiz_position - 1 === quizData.quiz_questions.length ? 1 : 0 
 
         return (
             <div>
                 {isQuizEnd ? <QuizEnd  /> :
-                <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />}
+                <QuizQuestion 
+                    quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} 
+                    showNextQuestionHandler={this.showNextQuestion.bind(this)}
+                />}
             </div>
         )
     }
